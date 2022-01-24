cd891c96e30b9c0caa7e57e3aabfd20a8075fac7
Prathibha <pratibha.indel@gmail.com>
2022-01-23 22:25:33 -0500

    saves the value of perticular hourblock

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var dayDisplayEl = $('#currentDay');
 //const today = moment().format("dddd, MMMM Do, YYYY"); // Current day
 const currentTime = moment(); //Current time
-const currentHour = moment().format("H");//time in hour
+const currentHour = moment().format("k");//time in hour
 
 
 
@@ -21,7 +21,7 @@ $(taskInput).each(function() {
 
    const taskInputNumber = parseInt($(this).attr("id"));//collecting id's
 
-   
+   //adding class to each hourblock
 if (taskInputNumber < currentHour) {
    $(this).addClass("past")}
    else if(taskInputNumber==currentHour){
@@ -34,25 +34,23 @@ else if (taskInputNumber > currentHour) {
 
 });
 
-
-
-
-//Save task data provided by user when clicking Save button.
-$(".saveBtn").click(function() {
-    event.preventDefault();
-    let hrTask = $(this).attr("hour")      // Corresponds to the key referenced for localStorage - also same as hour in military time.
-    let storeTask = $(this).prev().val();  // Previous sibling https://api.jquery.com/prev/ (in this case previous sibling is the textarea, value is the text entered.)
-    localStorage.setItem(hrTask, JSON.stringify(storeTask));
+//save button to save data
+$(".saveBtn").click(function(event) {
+   event.preventDefault();
+   let hrTask = $(this).attr("hour")      // Corresponds to the key referenced for localStorage - also same as hour in military time.
+   let storeTask = $(this).prev().val();  // Previous sibling https://api.jquery.com/prev/ (in this case previous sibling is the textarea, value is the text entered.)
+   localStorage.setItem(hrTask,storeTask);
 });
 
-// Recall tasks from localStorage back into scheduler when renavigating to the page or when page is refreashed.
 
-$("#9").val(localStorage.getItem("9"))
-$("#10").val(localStorage.getItem("10"))
-$("#11").val(localStorage.getItem("11"))
-$("#12").val(localStorage.getItem("12"))
-$("#13").val(localStorage.getItem("13"))
-$("#14").val(localStorage.getItem("14"))
-$("#15").val(localStorage.getItem("15"))
-$("#16").val(localStorage.getItem("16"))
-$("#17").val(localStorage.getItem("17")) 
+
+$("#9").val(localStorage.getItem(9));
+$("#10").val(localStorage.getItem(10));
+$("#11").val(localStorage.getItem(11));
+$("#12").val(localStorage.getItem(12));
+$("#13").val(localStorage.getItem(13));
+$("#14").val(localStorage.getItem(14));
+$("#15").val(localStorage.getItem(15));
+$("#16").val(localStorage.getItem(16));
+$("#17").val(localStorage.getItem(17)) ;
+
